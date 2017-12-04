e354ef659c79bf7f21c3c314adb2f68462ad0023
Don <wei_don@yahoo.com>
2017-12-04 09:27:26 -0500

    variable declaration and output

--- test1.adb
+++ test1.adb
@@ -1,6 +1,15 @@
-with Ada.Text_IO;
-use Ada.Text_IO;
+with Ada.Text_IO;			use Ada.Text_IO;
+with Ada.Integer_Text_IO;	use Ada.Integer_Text_IO;
 procedure Test1 is
+  --varialbe declaration
+  A,B : Integer:=0;
+  C : Integer:=5;
+  D : Integer:=20;
 begin
- Put_line("Hello World!");
+   
+  D :=D+1;
+  Put_line("Hello World!");
+  Put("D is");
+  PUt(D);
+  New_line(1);
 end Test1;
